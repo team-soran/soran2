c3d63184aa1d0c6de30f075ea8e618434e60c1c2
Kang HyoJun <admire9@gmail.com>
2013-08-06 01:02:14 +0900

    Add structure for flask

---
+++ noisy.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+
+from flask.ext.script import Manager
+
+from soran.web.app import app
+
+@Manager
+def manager(config=None):
+    config = os.path.abspath(config)
+    app.config.from_pyfile(config)
+    return app
+
+@manager.option('--host', dest='host', default=None)
+def sound(host):
+    app.run(host=host)
+
+manager.add_option('-c', '--config', dest='config', required=False)
+
+if __name__ == '__main__':
+    manager.run()
---
+++ setup.py
@@ -0,0 +1,12 @@
+#! -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(name='soran',
+      version='0.0.1',
+      author='Kang Hyojun',
+      author_email='hyojun@admire.kr',
+      install_requires=[
+          'flask==0.10.1', 'flask-script==0.5.3'
+      ])
+
+
