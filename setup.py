a9e1a0f7e8231005875663a4add3d0075addb128
Kang HyoJun <admire9@gmail.com>
2013-08-06 01:10:30 +0900

    Add extra libs

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ setup(name='soran',
       author='Kang Hyojun',
       author_email='hyojun@admire.kr',
       install_requires=[
-          'flask==0.10.1', 'flask-script==0.5.3'
+          'flask==0.10.1', 'flask-script==0.5.3', 'sqlalchemy==0.8.2',
+          'Flask-OAuthlib==0.3.0', 'alembic==0.6.0', 'pytest==2.3.5'
       ])
 
 
