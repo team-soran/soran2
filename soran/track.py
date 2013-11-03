a7232f359e7023d607a7ae2823da78e75a006358
Kang Hyojun <admire9@gmail.com>
2013-11-03 22:06:10 +0900

    Missing conflict

--- soran/track.py
+++ soran/track.py
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-from sqlalchemy import Column, Integer, Unicode
-
-from .db import Base, services
-
-=======
 from datetime import datetime
 
 from sqlalchemy import Column, Integer, Unicode, DateTime
@@ -12,7 +6,6 @@ from .db import Base, services
 
 __all__ = 'Track',
 
->>>>>>> 13ba6f7c091faee853a927b17de0c1346bd49431
 class Track(Base):
     id = Column(Integer, primary_key=True)
 
@@ -22,11 +15,8 @@ class Track(Base):
 
     services = Column(services, nullable=False)
 
-<<<<<<< HEAD
-=======
     created_at = Column(DateTime(timezone=True),
                         default=datetime.utcnow,
                         nullable=False)
 
->>>>>>> 13ba6f7c091faee853a927b17de0c1346bd49431
     __tablename__ = 'tracks'
