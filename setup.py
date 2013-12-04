c0706346b972d7a5fdb2829c371e6c4933d7c40c
Kang Hyojun <hyojun@admire.kr>
2013-12-04 19:53:03 +0900

    Version up sqlalchemy

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(name='soran',
       author='Kang Hyojun',
       author_email='hyojun@admire.kr',
       install_requires=[
-          'flask==0.10.1', 'flask-script==0.5.3', 'sqlalchemy==0.8.2',
+          'flask==0.10.1', 'flask-script==0.5.3', 'sqlalchemy==0.8.3',
           'Flask-OAuthlib==0.3.0', 'alembic==0.6.0', 'pytest==2.3.5',
           'py-bcrypt==0.4'
       ])
